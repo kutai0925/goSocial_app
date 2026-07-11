1ba744a90487e21961e68df2ab5334b8bed42f59
dubleidd <nicobremicker@icloud.com>
2026-07-11 21:19:19 +0200

    + some comments

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -18,17 +18,20 @@ struct User {
 
 #[tokio::main]
 async fn main() {
-    let shared_state = AppState::default();
+    let shared_state = AppState::default(); 
 
+    // create the router and set the handler for paths
     let app = Router::new()
         .route("/v1/users", post(try_add_user))
         .with_state(shared_state);
 
+    // creating the listener and binding it to the expo address
     let listener = tokio::net::TcpListener::bind("127.0.0.1:8081").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
 
+// handler for the user creation TODO: add Uuid return and check if user exists
 async fn try_add_user(State(state): State<AppState>, Json(payload): Json<User>) {
     let user_id = Uuid::new_v4();
 
