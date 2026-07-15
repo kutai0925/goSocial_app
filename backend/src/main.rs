e08dad36ceb61aefa4b83f8a6d26d8d7f928d0d3
dubleidd <nicobremicker@icloud.com>
2026-07-15 15:16:04 +0200

    + login, ~ changes names from HashSet to HashMap

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,13 +1,13 @@
-use std::{collections::{HashMap, HashSet}, sync::{Arc, Mutex}};
+use std::{collections::HashMap, sync::{Arc, Mutex}};
 
-use axum::{Json, Router, extract::State, http::StatusCode, routing::post};
+use axum::{Json, Router, extract::State, http::StatusCode, routing::{post, put}};
 use serde::Deserialize;
 use uuid::Uuid;
 
 #[derive(Default, Clone)]
 struct AppState {
     users: Arc<Mutex<HashMap<Uuid, User>>>,
-    names: Arc<Mutex<HashSet<String>>>
+    names: Arc<Mutex<HashMap<String, Uuid>>>
 }
 
 #[derive(Deserialize, Clone)]
@@ -24,6 +24,7 @@ async fn main() {
     // create the router and set the handler for paths
     let app = Router::new()
         .route("/v1/users", post(try_add_user))
+        .route("/v1/users/login", put(try_login))
         .with_state(shared_state);
 
     // creating the listener and binding it to the expo address
@@ -32,11 +33,14 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-// handler for the user creation TODO: add Uuid return and check if user exists
+/// handler for the user creation 
+/// uses the Json payload to create a user
+/// if the username in the payload already exists, returns StatusCode 400 (BadRequest)
+/// else creates the user and returns the Uuid in Json
 async fn try_add_user(State(state): State<AppState>, Json(payload): Json<User>) 
 -> Result<Json<Uuid>, StatusCode> {
     // checks if the username already exists, if it does returns StatusCode 400
-    if !state.names.lock().unwrap().contains(&payload.username) {
+    if state.names.lock().unwrap().contains_key(&payload.username) {
         return Err(StatusCode::BAD_REQUEST)
     }
 
@@ -51,9 +55,36 @@ async fn try_add_user(State(state): State<AppState>, Json(payload): Json<User>)
     };
 
     // inserts the username so we know it exists
-    state.names.lock().unwrap().insert(user.username.clone());
+    state.names.lock().unwrap().insert(user.username.clone(), user_id);
     // adds the user to the hashmap
     state.users.lock().unwrap().insert(user_id, user.clone());
 
     Ok(Json(user.user_id))
+}
+
+/// handler for the login
+/// uses the payload to check if the login is valid
+/// returns the Uuid in json if valid
+async fn try_login(State(state): State<AppState>, Json(payload): Json<User>) 
+-> Result<Json<Uuid>, StatusCode> {
+    // checks if the username exists, if it does not returns StatusCode 400
+    if !state.names.lock().unwrap().contains_key(&payload.username) {
+        return Err(StatusCode::BAD_REQUEST)
+    }
+
+    // gets the Uuid from the username, returns StatusCode 500 if fail
+    let &user_id = match state.names.lock().unwrap().get(&payload.username) {
+        Some(x) => x,
+        None => return Err(StatusCode::INTERNAL_SERVER_ERROR)
+    };
+
+    // matches the password in the payload with the saved password, 
+    // if same just continues, else responds with StatusCode 400
+    match &state.users.lock().unwrap().get(&user_id).unwrap().password_hash {
+        s if *s == payload.password_hash => {},
+        _ => return Err(StatusCode::BAD_REQUEST)
+    }
+
+    // if it gets till here its a valid login, thus returns the user_id
+    Ok(Json(user_id))
 }
