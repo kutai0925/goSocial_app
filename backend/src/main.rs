008a9ced593c49041354fd875bb43d546cc14555
dubleidd <nicobremicker@icloud.com>
2026-07-15 17:11:53 +0200

    + user deletion, + get user based on id, + set user location

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,20 +1,30 @@
 use std::{collections::HashMap, sync::{Arc, Mutex}};
 
-use axum::{Json, Router, extract::State, http::StatusCode, routing::{post, put}};
-use serde::Deserialize;
+use axum::{Json, Router, extract::{Path, State}, http::StatusCode, routing::{delete, post, put, get}};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+// shared App state used by the handlers to get information requried
 #[derive(Default, Clone)]
 struct AppState {
     users: Arc<Mutex<HashMap<Uuid, User>>>,
     names: Arc<Mutex<HashMap<String, Uuid>>>
 }
 
-#[derive(Deserialize, Clone)]
+#[derive(Serialize, Deserialize, Clone)]
 struct User {
     username: String,
+    #[serde(skip_serializing)]
     password_hash: String,
-    user_id: Uuid
+    user_id: Uuid,
+    coordinates: Coordinates
+}
+
+#[derive(Serialize, Deserialize, Default, Clone)]
+struct Coordinates {
+    lat: f64,
+    lon: f64,
+    accuracy: i8
 }
 
 #[tokio::main]
@@ -25,6 +35,9 @@ async fn main() {
     let app = Router::new()
         .route("/v1/users", post(try_add_user))
         .route("/v1/users/login", put(try_login))
+        .route("/v1/users/{user_id}", delete(del_user))
+        .route("/v1/users/{user_id}/coordinates", put(try_set_location))
+        .route("/v1/users/{user_id}", get(try_get_user))
         .with_state(shared_state);
 
     // creating the listener and binding it to the expo address
@@ -33,10 +46,15 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
+//***********************************************************************************************************
+// Start of Account Handling
+//***********************************************************************************************************
+
 /// handler for the user creation 
 /// uses the Json payload to create a user
 /// if the username in the payload already exists, returns StatusCode 400 (BadRequest)
 /// else creates the user and returns the Uuid in Json
+#[axum::debug_handler]
 async fn try_add_user(State(state): State<AppState>, Json(payload): Json<User>) 
 -> Result<Json<Uuid>, StatusCode> {
     // checks if the username already exists, if it does returns StatusCode 400
@@ -48,11 +66,7 @@ async fn try_add_user(State(state): State<AppState>, Json(payload): Json<User>)
     let user_id = Uuid::new_v4();
 
     // creates the user
-    let user = User {
-        username: payload.username,
-        password_hash: payload.password_hash,
-        user_id
-    };
+    let user = User::from(payload.username, payload.password_hash, user_id);
 
     // inserts the username so we know it exists
     state.names.lock().unwrap().insert(user.username.clone(), user_id);
@@ -65,6 +79,7 @@ async fn try_add_user(State(state): State<AppState>, Json(payload): Json<User>)
 /// handler for the login
 /// uses the payload to check if the login is valid
 /// returns the Uuid in json if valid
+#[axum::debug_handler]
 async fn try_login(State(state): State<AppState>, Json(payload): Json<User>) 
 -> Result<Json<Uuid>, StatusCode> {
     // checks if the username exists, if it does not returns StatusCode 400
@@ -87,4 +102,106 @@ async fn try_login(State(state): State<AppState>, Json(payload): Json<User>)
 
     // if it gets till here its a valid login, thus returns the user_id
     Ok(Json(user_id))
+}
+
+/// handler to delete a user
+/// path is used to get the userid
+async fn del_user(State(state): State<AppState>, Path(path): Path<String>) -> StatusCode {
+    // parses the path into a Uuid to use, returns SatusCode 500 on fail
+    let user_id = match Uuid::parse_str(&path) {
+        Ok(id) => id,
+        Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    let username = state.users.lock().unwrap().get(&user_id).unwrap().username.clone();
+    // removes the name from the already used names, returns StatusCode 500 on fail
+    match state.names.lock().unwrap().remove(&username) {
+        Some(_) => {},
+        None => return StatusCode::INTERNAL_SERVER_ERROR
+    }
+
+    // removes the user from the users, returns StatusCode 400 on fail
+    match state.users.lock().unwrap().remove(&user_id) {
+        Some(_) => {},
+        None => return StatusCode::BAD_REQUEST
+    }
+
+    StatusCode::OK
+}
+//***********************************************************************************************************
+// End of Account Handling
+//***********************************************************************************************************
+
+//***********************************************************************************************************
+// Start of User data requests Handling
+//***********************************************************************************************************
+
+/// handler for getting a specific user based on user_id
+/// path contains the user_id
+/// returns the user as json if successful
+async fn try_get_user(State(state): State<AppState>, Path(path): Path<String>) 
+-> Result<Json<User>, StatusCode> {
+    // parses the path into a Uuid to use, returns SatusCode 500 on fail
+    let user_id = match Uuid::parse_str(&path) {
+        Ok(id) => id,
+        Err(_) => return Err(StatusCode::INTERNAL_SERVER_ERROR),
+    };
+
+    // returns the user as json if exists, else returns StatusCode 400 on fail
+    match state.users.lock().unwrap().get(&user_id) {
+        Some(u) => return Ok(Json(u.clone())),
+        None => return Err(StatusCode::BAD_REQUEST)
+    }
+}
+
+//***********************************************************************************************************
+// Start of User data requests Handling
+//***********************************************************************************************************
+
+//***********************************************************************************************************
+// Start of Location Handling
+//***********************************************************************************************************
+
+// handler to set the location of the current user
+// path is used for the id of the user to update the location of
+// payload contains the location
+#[axum::debug_handler]
+async fn try_set_location(State(state): State<AppState>, 
+Path(path): Path<String>, 
+Json(payload): Json<Coordinates>) 
+-> StatusCode {
+
+    // parses the path into a Uuid to use, returns SatusCode 500 on fail
+    let user_id = match Uuid::parse_str(&path) {
+        Ok(id) => id,
+        Err(_) => return StatusCode::INTERNAL_SERVER_ERROR,
+    };
+
+    // gets the user assositate with the user_id sent in the path, and sets the new Coordiantes 
+    // returns StatusCode 400 if it fails
+    match state.users.lock().unwrap().get_mut(&user_id) {
+        Some(x) => x.coordinates = Coordinates { 
+            lat: payload.lat, 
+            lon: payload.lon, 
+            accuracy: payload.accuracy },
+        None => return StatusCode::BAD_REQUEST
+    };
+
+    // returns 200
+    StatusCode::OK
+}
+
+//***********************************************************************************************************
+// End of Location Handling
+//***********************************************************************************************************
+
+impl User {
+    fn from(username: String, password_hash: String, user_id: Uuid) -> Self {
+        Self {
+            username,
+            password_hash,
+            user_id,
+            coordinates: Coordinates::default()
+        }
+    }
 }
